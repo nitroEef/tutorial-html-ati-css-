787282510a5decf5ed565c04524ddfab8cf6f8ab
nitroEef <mikebodetelli@gmail.com>
2024-02-20 07:34:47 +0100

    first commit

---
+++ lesson12/script.js
@@ -0,0 +1,23 @@
+let costs = 15
+let profits ;
+let salesPrices = 20
+
+profits = salesPrices - costs;
+console.log(profit);
+
+// plus for number
+let cost = 15 , profit = 5 , salesPrice = cost + profit;
+console.log (salesPrice);
+
+// plus for string
+let firstName = "Olatunde", surname = "tobi ", fullname = surname + firstName;
+
+// backtick ``
+console.log (`${fullname} + "is a very big boy"`)
+
+let food = "rice"
+console.log(`what will i eat, if not ${food}?`);
+
+let a = 2
+a *= 4
+console.log(a)
